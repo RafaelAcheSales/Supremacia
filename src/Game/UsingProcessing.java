1272050b19234a7aec128b1b380058223c1739b2
Bryan Lima <bryan.lima@hotmail.com>
2020-09-27 17:10:15 -0300

    adicionando mais territorios e mares

--- src/Game/UsingProcessing.java
+++ src/Game/UsingProcessing.java
@@ -102,60 +102,185 @@ public class UsingProcessing extends PApplet {
         territorios.add(greenland);
     }
     public void setupAfrica(){
-        ArrayList<String> frontAlaska = new ArrayList<>(Arrays.asList("Nothern Canada",
-                "Western Canada","North Pacific", "Barkley Sound"));
-        Territorio alaska = new Territorio(this, "Alaska", frontAlaska,
-                new PVector(180, 180), 60, 30,null);
-        territorios.add(alaska);
+        ArrayList<String> frontSahara = new ArrayList<>(Arrays.asList("Mid Atlantic",
+                "Mediterranean Sea","Egypt", "Nigeria"));
+        Territorio sahara = new Territorio(this, "Sahara", frontSahara,
+                new PVector(711, 507), 60, 30,null);
+        territorios.add(sahara);
+
+        ArrayList<String> frontEgypt = new ArrayList<>(Arrays.asList("Sahara",
+                "Mediterranean Sea","Red Sea", "Nigeria","Mozambique"));
+        Territorio egypt = new Territorio(this, "Egypt", frontEgypt,
+                new PVector(844, 527), 60, 30,null);
+        territorios.add(egypt);
+
+        ArrayList<String> frontNigeria = new ArrayList<>(Arrays.asList("Sahara",
+                "Egypt","Gulf of Guinea", "Zaire","Mozambique","Mid Atlantic"));
+        Territorio nigeria = new Territorio(this, "Nigeria", frontNigeria,
+                new PVector(756, 578), 60, 30,null);
+        territorios.add(nigeria);
+
+        ArrayList<String> frontZaire = new ArrayList<>(Arrays.asList("South Africa",
+                "Gulf of Guinea", "Nigeria","Mozambique","Cape of Good Hope"));
+        Territorio zaire = new Territorio(this, "Zaire", frontZaire,
+                new PVector(832, 693), 60, 30,null);
+        territorios.add(zaire);
+
+        ArrayList<String> frontMozambique = new ArrayList<>(Arrays.asList("South Africa",
+                "Straits of Malacca", "Nigeria","Zaire","Red Sea","Egypt"));
+        Territorio mozambique = new Territorio(this, "Mozambique", frontMozambique,
+                new PVector(931, 654), 80, 30,null);
+        territorios.add(mozambique);
+
+        ArrayList<String> frontSouthAfrica = new ArrayList<>(Arrays.asList("Mozambique",
+                "Straits of Malacca", "Cape of Good Hope","Zaire"));
+        Territorio southAfrica = new Territorio(this, "Mozambique", frontSouthAfrica,
+                new PVector(866, 772), 80, 30,null);
+        territorios.add(southAfrica);
+
+    }
+    public void setupRussia(){
+        ArrayList<String> frontKola = new ArrayList<>(Arrays.asList("Barents Sea",
+                "Scandinavia","Rusia", "Siberia"));
+        Territorio kola = new Territorio(this, "Kola", frontKola,
+                new PVector(1004, 180), 40, 30,null);
+        territorios.add(kola);
+
+        ArrayList<String> frontRussia = new ArrayList<>(Arrays.asList("Baltic Sea",
+                "Scandinavia","Baltic Sea", "Siberia","Poland","Romania","Kazakh","Buryatsk","Kola"));
+        Territorio russia = new Territorio(this, "Russia", frontRussia,
+                new PVector(989, 247), 60, 30,null);
+        territorios.add(russia);
+
+        ArrayList<String> frontKazakh = new ArrayList<>(Arrays.asList("Black Sea",
+                "Manchuria","Mongolia", "Tibet","Pakistan","Romania","Russia","Buryatsk",
+                "Black Sea"));
+        Territorio kazakh = new Territorio(this, "Kazakh", frontKazakh,
+                new PVector(1074, 316), 60, 30,null);
+        territorios.add(kazakh);
+
+        ArrayList<String> frontSiberia = new ArrayList<>(Arrays.asList("Barents Sea",
+                "Yakutsk","Kola","Russia","Buryatsk"));
+        Territorio siberia = new Territorio(this, "Siberia", frontSiberia,
+                new PVector(1152, 200), 60, 30,null);
+        territorios.add(siberia);
+
+        ArrayList<String> frontBuryatsk = new ArrayList<>(Arrays.asList("Sea of Okhotsk",
+                "Yakutsk","Sea of Japan","Russia","Siberia","Manchuria","Mongolia","Kazakh"));
+        Territorio buryatsk = new Territorio(this, "Buryatsk", frontBuryatsk,
+                new PVector(1224, 281), 60, 30,null);
+        territorios.add(buryatsk);
+
+        ArrayList<String> frontYakutsk = new ArrayList<>(Arrays.asList("Sea of Okhotsk",
+                "Buryatsk","North Pacific","Siberia"));
+        Territorio yakutsk = new Territorio(this, "Yakutsk", frontYakutsk,
+                new PVector(1314, 171), 60, 30,null);
+        territorios.add(yakutsk);
 
     }
     public void setupEurope(){
         ArrayList<String> frontBritishIsles = new ArrayList<>(Arrays.asList("North Sea"));
         Territorio britishIsles = new Territorio(this, "British Isles", frontBritishIsles,
-                new PVector(705, 246), 70, 30,null);
+                new PVector(705, 246), 70, 25,null);
         territorios.add(britishIsles);
 
         ArrayList<String> frontScandinavia = new ArrayList<>(Arrays.asList("North Sea",
                 "Barents Sea","Baltic Sea", "Kola","Russia"));
         Territorio scandinavia = new Territorio(this, "Scandinavia", frontScandinavia,
-                new PVector(785, 190), 90, 30,null);
+                new PVector(785, 190), 90, 25,null);
         territorios.add(scandinavia);
 
         ArrayList<String> frontPoland = new ArrayList<>(Arrays.asList("Eastern Europe",
                 "Greece","Romania", "Kola","Russia","Baltic Sea"));
         Territorio poland = new Territorio(this, "Poland", frontPoland,
-                new PVector(859, 262), 60, 30,null);
+                new PVector(859, 262), 60, 25,null);
         territorios.add(poland);
 
         ArrayList<String> frontIberia = new ArrayList<>(Arrays.asList("Western Europe",
                 "Bay of Biscay","Mediterranean Sea"));
         Territorio iberia = new Territorio(this, "Iberia", frontIberia,
-                new PVector(683, 374), 60, 30,null);
+                new PVector(683, 374), 60, 25,null);
         territorios.add(iberia);
 
         ArrayList<String> frontWesternEurope = new ArrayList<>(Arrays.asList("Iberia",
                 "Bay of Biscay","Mediterranean Sea","North Sea","Eastern Europe","Baltic Sea"));
         Territorio westernEurope = new Territorio(this, "Western Europe", frontWesternEurope,
-                new PVector(740, 323), 90, 30,null);
+                new PVector(740, 323), 90, 25,null);
         territorios.add(westernEurope);
 
         ArrayList<String> frontEasternEurope = new ArrayList<>(Arrays.asList("Poland",
                 "Greece","Mediterranean Sea","Western Europe","Baltic Sea"));
         Territorio easternEurope = new Territorio(this, "Eastern Europe", frontEasternEurope,
-                new PVector(809, 290), 90, 30,null);
+                new PVector(809, 290), 90, 25,null);
         territorios.add(easternEurope);
 
         ArrayList<String> frontGreece = new ArrayList<>(Arrays.asList("Poland",
                 "Romania","Sea of Crete","Black Sea","Eastern Europe"));
         Territorio greece = new Territorio(this, "Greece", frontGreece,
-                new PVector(851, 343), 60, 30,null);
+                new PVector(851, 343), 60, 25,null);
         territorios.add(greece);
 
         ArrayList<String> frontRomania = new ArrayList<>(Arrays.asList("Poland",
                 "Greece","Russia","Black Sea","Kazakh"));
         Territorio romania = new Territorio(this, "Romania", frontRomania,
-                new PVector(900, 305), 60, 30,null);
+                new PVector(900, 305), 60, 25,null);
         territorios.add(romania);
+
+    }
+    public void setupOceania(){
+        ArrayList<String> frontNewZealand = new ArrayList<>(Arrays.asList("Tasman Sea"));
+        Territorio newZealand = new Territorio(this, "New Zealand", frontNewZealand,
+                new PVector(1390, 853), 70, 25,null);
+        territorios.add(newZealand);
+
+        ArrayList<String> frontEasternAustralia = new ArrayList<>(Arrays.asList("Tasman Sea",
+                "Great Australian Blight","Western Australia","Timor Sea"));
+        Territorio easternAustralia = new Territorio(this, "Eastern Australia", frontEasternAustralia,
+                new PVector(1280, 839), 100, 30,null);
+        territorios.add(easternAustralia);
+
+        ArrayList<String> frontWesternAustralia = new ArrayList<>(Arrays.asList("Tasman Sea",
+                "Great Australian Blight","Eastern Australia","Shark Bay","Timor Sea"));
+        Territorio westernAustralia = new Territorio(this, "Western Australia", frontWesternAustralia,
+                new PVector(1180, 810), 100, 30,null);
+        territorios.add(westernAustralia);
+
+        ArrayList<String> frontIndonesia = new ArrayList<>(Arrays.asList("Java Sea",
+                "Mid Pacific","Bay of Bengal","Timor Sea"));
+        Territorio indonesia = new Territorio(this, "Indonesia", frontIndonesia,
+                new PVector(1310, 670), 100, 25,null);
+        territorios.add(indonesia);
+    }
+    public void setupMiddleEast(){
+        ArrayList<String> frontMiddleEast = new ArrayList<>(Arrays.asList("Sea of Crete",
+                "Red Sea","Arabia","Iraq","Turkey"));
+        Territorio middleEast = new Territorio(this, "Middle East", frontMiddleEast,
+                new PVector(919, 465), 70, 25,null);
+        territorios.add(middleEast);
+
+        ArrayList<String> frontArabia = new ArrayList<>(Arrays.asList("Middle East",
+                "Red Sea","Arabian Sea","Iraq"));
+        Territorio arabia = new Territorio(this, "Arabia", frontArabia,
+                new PVector(996, 545), 50, 25,null);
+        territorios.add(arabia);
+
+        ArrayList<String> frontIraq = new ArrayList<>(Arrays.asList("Middle East",
+                "Arabia","Arabian Sea","Turkey","Iran"));
+        Territorio iraq = new Territorio(this, "Iraq", frontIraq,
+                new PVector(1004, 454), 40, 25,null);
+        territorios.add(iraq);
+
+        ArrayList<String> frontIran = new ArrayList<>(Arrays.asList("Pakistan",
+                "Arabian Sea","Turkey","Iraq"));
+        Territorio iran = new Territorio(this, "Iran", frontIran,
+                new PVector(1056, 444), 40, 25,null);
+        territorios.add(iran);
+
+        ArrayList<String> frontTurkey = new ArrayList<>(Arrays.asList("Pakistan",
+                "Black Sea","Iran","Iraq","Pakistan","Kazakh"));
+        Territorio turkey = new Territorio(this, "Turkey", frontTurkey,
+                new PVector(1056, 444), 40, 25,null);
+        territorios.add(turkey);
 
     }
     public void setupMarAzulClaro(){
@@ -236,7 +361,7 @@ public class UsingProcessing extends PApplet {
         ArrayList<String> frontBalticSea = new ArrayList<>(Arrays.asList("Scandinavia",
                 "Russia", "Poland","North Sea","Eastern Europe","Western Europe"));
         MarAzulClaro balticSea = new MarAzulClaro(this, "Baltic Sea", frontBalticSea,
-                new PVector(861, 205), 60, 30,null);
+                new PVector(861, 200), 60, 25,null);
         territorios.add(balticSea);
 
         ArrayList<String> frontBayOfBiscay = new ArrayList<>(Arrays.asList("North Sea",
@@ -253,6 +378,106 @@ public class UsingProcessing extends PApplet {
                 new PVector(702, 426), 130, 30,null);
         territorios.add(mediterraneanSea);
 
+        ArrayList<String> frontSeaOfCrete = new ArrayList<>(Arrays.asList("Black Sea",
+                "Mediterranean Sea","Red Sea","Eastern Europe","Greece","Middle East","Turkey"));
+        MarAzulClaro seaOfCrete = new MarAzulClaro(this, "Sea of Crete", frontSeaOfCrete,
+                new PVector(862, 418), 90, 30,null);
+        territorios.add(seaOfCrete);
+
+        ArrayList<String> frontBlackSea = new ArrayList<>(Arrays.asList("Sea of Crete",
+                "Romania","Kazakh","Greece","Turkey"));
+        MarAzulClaro blackSea = new MarAzulClaro(this, "Black Sea", frontBlackSea,
+                new PVector(966, 338), 70, 30,null);
+        territorios.add(blackSea);
+        // Africa
+        ArrayList<String> frontGulfOfGuinea = new ArrayList<>(Arrays.asList("Mid Atlantic",
+                "South Atlantic","Nigeria","Zaire","Cape of Good Hope"));
+        MarAzulClaro golfOfGuinea = new MarAzulClaro(this, "Gulf of Guinea", frontGulfOfGuinea,
+                new PVector(713, 637), 90, 30,null);
+        territorios.add(golfOfGuinea);
+
+        ArrayList<String> frontCapeOfGoodHope = new ArrayList<>(Arrays.asList("Straits of Malacca",
+                "South Atlantic","Indian Ocean","Zaire","Gulf of Guinea","South Africa"));
+        MarAzulClaro capeOfGoodHope = new MarAzulClaro(this, "Cape of Good Hope", frontCapeOfGoodHope,
+                new PVector(846, 834), 110, 30,null);
+        territorios.add(capeOfGoodHope);
+
+        ArrayList<String> frontStraitsOfMalacca = new ArrayList<>(Arrays.asList("Cape of Good Hope",
+                "Mozambique","Indian Ocean","Red Sea","South Africa"));
+        MarAzulClaro straitsOfMalacca = new MarAzulClaro(this, "Straits of Malacca", frontStraitsOfMalacca,
+                new PVector(973, 713), 110, 30,null);
+        territorios.add(straitsOfMalacca);
+
+        ArrayList<String> frontRedSea = new ArrayList<>(Arrays.asList("Straits of Malacca",
+                "Mozambique","Egypt","Arabia","Arabian Sea","Middle East","Sea of Crete","Mediterranean Sea"));
+        MarAzulClaro redSea = new MarAzulClaro(this, "Red Sea", frontRedSea,
+                new PVector(920, 540), 60, 25,null);
+        territorios.add(redSea);
+
+        //Russia & China
+        ArrayList<String> frontSeaOfOkhotsk = new ArrayList<>(Arrays.asList("North Pacific",
+                "Yakutsk","Buryatsk","Japan","Sea of Japan","Tokyo Bay"));
+        MarAzulClaro seaOfOkhotsk = new MarAzulClaro(this, "Sea of Okhotsk", frontSeaOfOkhotsk,
+                new PVector(1350, 251), 110, 25,null);
+        territorios.add(seaOfOkhotsk);
+
+        ArrayList<String> frontTokyoBay = new ArrayList<>(Arrays.asList("North Pacific",
+                "Sea of Okhotsk","Japan","Sea of Japan"));
+        MarAzulClaro tokyoBay = new MarAzulClaro(this, "Tokyo Bay", frontTokyoBay,
+                new PVector(1416, 330), 60, 30,null);
+        territorios.add(tokyoBay);
+
+        ArrayList<String> frontSeaOfJapan = new ArrayList<>(Arrays.asList("North Pacific",
+                "Sea of Okhotsk","Japan","Tokyo Bay","Manchuria","Mongolia","Shantung",
+                "Nanling","South China Sea","Mid Pacific"));
+        MarAzulClaro seaOfJapan = new MarAzulClaro(this, "Sea of Japan", frontSeaOfJapan,
+                new PVector(1370, 415), 80, 25,null);
+        territorios.add(seaOfJapan);
+
+        ArrayList<String> frontSouthChinaSea = new ArrayList<>(Arrays.asList("Mid Pacific",
+                "Sea of Japan","Indo-China","Java Sea","Nanling"));
+        MarAzulClaro southChinaSea = new MarAzulClaro(this, "South China Sea", frontSouthChinaSea,
+                new PVector(1379, 476), 100, 25,null);
+        territorios.add(southChinaSea);
+
+        // Oceania
+        ArrayList<String> frontTasmanSea = new ArrayList<>(Arrays.asList("South Pacific",
+                "Timor Sea","New Zealand","Eastern Australia","Great Australian Blight"));
+        MarAzulClaro tasmanSea = new MarAzulClaro(this, "Tasman Sea", frontTasmanSea,
+                new PVector(1338, 941), 70, 25,null);
+        territorios.add(tasmanSea);
+
+        ArrayList<String> frontTimorSea = new ArrayList<>(Arrays.asList("South Pacific",
+                "Tasman Sea","Western Australia","Eastern Australia","Mid Pacific",
+                "Shark Bay","Bay of Bengal","Indonesia"));
+        MarAzulClaro timorSea = new MarAzulClaro(this, "Timor Sea", frontTimorSea,
+                new PVector(1262, 705), 70, 25,null);
+        territorios.add(timorSea);
+
+        ArrayList<String> frontTSharkBay = new ArrayList<>(Arrays.asList("Indian Ocean",
+                "Timor Sea","Western Australia","Great Australian Blight"));
+        MarAzulClaro sharkBay = new MarAzulClaro(this, "Shark Bay", frontTSharkBay,
+                new PVector(1123, 780), 70, 25,null);
+        territorios.add(sharkBay);
+
+        //India - Oriente
+        ArrayList<String> frontArabianSea = new ArrayList<>(Arrays.asList("Indian Ocean",
+                "Red Sea","Arabia","Iraq","Iran","Pakistan","India","Bay of Bengal"));
+        MarAzulClaro arabianSea = new MarAzulClaro(this, "Arabian Sea", frontArabianSea,
+                new PVector(1097, 572), 70, 25,null);
+        territorios.add(arabianSea);
+
+        ArrayList<String> frontBayOfBengal = new ArrayList<>(Arrays.asList("Indian Ocean",
+                "Arabian Sea","Burma","India","Java Sea","Timor Sea","Indonesia"));
+        MarAzulClaro bayOfBengal = new MarAzulClaro(this, "Bay of Bengal", frontBayOfBengal,
+                new PVector(1142, 627), 90, 25,null);
+        territorios.add(bayOfBengal);
+
+        ArrayList<String> frontJavaSea = new ArrayList<>(Arrays.asList("Mid Pacific",
+                "Indo-China","Burma","South China Sea","Bay of Bengal","Indonesia"));
+        MarAzulClaro javaSea = new MarAzulClaro(this, "Java Sea", frontJavaSea,
+                new PVector(1328, 616), 60, 25,null);
+        territorios.add(javaSea);
 
     }
     public void setupMarAzulEscuro(){
@@ -328,6 +553,14 @@ public class UsingProcessing extends PApplet {
         setupMarAzulEscuro();
         //Europa
         setupEurope();
+        //Africa
+        setupAfrica();
+        //Russia
+        setupRussia();
+        //Oceania
+        setupOceania();
+        // Middle East
+        setupMiddleEast();
         
     }
 
